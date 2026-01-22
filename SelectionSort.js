9ee1339a5c570e49dd9726c1b5bd3a07af0dceee
lussiie <luss.aghababyan@gmail.com>
2026-01-22 21:06:43 +0400

    done

---
+++ SelectionSort.js
@@ -0,0 +1,25 @@
+function selectionSort(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+
+  return arr;
+}
+
+
+let numbers = [22,1,33,54,2,3];
+console.log(selectionSort(numbers));
